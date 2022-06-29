a69a346f67fd051fb5ae3c4183a1d221d4e358f8
Víctor Adrián Sosa Hernández <47755333+vsosahdz@users.noreply.github.com>
2022-06-29 01:19:52 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -71,4 +71,4 @@ def modelo():
     return jsonify({"Resultado":str(resultado[0])})
 
 if __name__ == '__main__':
-    servidorWeb.run(debug=False,host='0.0.0.0',port='8082')
+    servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
